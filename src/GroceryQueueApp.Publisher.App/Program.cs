d8a58d1c0f95637bb00b313f7492be4c8d64ae37
Matheus de Lima Santana <matheus.lima@take.net>
2023-09-19 22:14:07 -0300

    Add project files.

---
+++ src/GroceryQueueApp.Publisher.App/Program.cs
@@ -0,0 +1,57 @@
+﻿using RabbitMQ.Client;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+    SetupBusChannel(channel);
+
+    /*
+     1 - fast-queue
+     2 - normal-queue
+     3 - preferential-queue
+    */
+
+    var groceryQueues = new string[] { "fast-queue", "normal-queue", "preferential-queue" };
+    var random = new Random();
+
+    while (true)
+    {
+        Console.WriteLine("Press any key to generate more messages");
+        Console.ReadLine();
+
+
+    }
+
+
+    var message = "fast-queue";
+
+    var body = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish(exchange: "grocery",
+                         routingKey: string.Empty,
+                         basicProperties: null,
+                         body: body);
+
+    Console.WriteLine($" [x] Sent {message}");
+
+    Console.WriteLine(" Press [enter] to exit.");
+    Console.ReadLine();
+}
+
+IModel SetupBusChannel(IModel channel)
+{
+
+    {
+        channel.QueueDeclare(queue: "fast-queue", durable: true, exclusive: false, autoDelete: false);
+        channel.QueueDeclare(queue: "normal-queue", durable: false, exclusive: false, autoDelete: false);
+        channel.QueueDeclare(queue: "preferential-queue", durable: false, exclusive: false, autoDelete: false);
+
+        channel.ExchangeDeclare("grocery", "direct");
+
+        return channel;
+    }
+}
