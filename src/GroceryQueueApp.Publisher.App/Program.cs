590fbe2b7f34a56b27103906d1f9cce671de816d
Matheus de Lima Santana <math3226@gmail.com>
2023-09-21 20:59:18 -0300

    Topic Exchange

--- src/GroceryQueueApp.Publisher.App/Program.cs
+++ src/GroceryQueueApp.Publisher.App/Program.cs
@@ -6,7 +6,7 @@ var factory = new ConnectionFactory { HostName = "localhost" };
 using (var connection = factory.CreateConnection())
 using (var channel = connection.CreateModel())
 {
-    SetupBusChannel(channel);
+    var (type, exchangeName)= SetupBusChannel(channel);
 
     /*
      1 - fast-queue
@@ -14,21 +14,23 @@ using (var channel = connection.CreateModel())
      3 - preferential-queue
     */
 
-    var groceryQueues = new string[] { "fast", "normal", "preferential" };
+    var groceryQueues = new List<string>() { "fast", "normal", "preferential" };
     var random = new Random();
+
+    if (type.Equals(ExchangeType.Topic)) groceryQueues.Add("normal.busy");
 
     while (true)
     {
         Console.WriteLine("Press any key to generate more messages");
         Console.ReadLine();
 
-        var routingKey = groceryQueues[random.Next(0, groceryQueues.Length)];
+        var routingKey = groceryQueues[random.Next(0, groceryQueues.Count)];
 
         var message = $"This Consumer has a message for queue {routingKey}";
 
         var body = Encoding.UTF8.GetBytes(message);
 
-        channel.BasicPublish(exchange: "grocery",
+        channel.BasicPublish(exchange: exchangeName,
                              routingKey: routingKey,
                              basicProperties: null,
                              body: body);
@@ -37,17 +39,24 @@ using (var channel = connection.CreateModel())
     }
 }
 
-IModel SetupBusChannel(IModel channel)
+(string, string) SetupBusChannel(IModel channel)
 {
     channel.QueueDeclare(queue: "fast-queue", durable: true, exclusive: false, autoDelete: false);
-    channel.QueueDeclare(queue: "normal-queue", durable: false, exclusive: false, autoDelete: false);
-    channel.QueueDeclare(queue: "preferential-queue", durable: false, exclusive: false, autoDelete: false);
+    channel.QueueDeclare(queue: "normal-queue", durable: true, exclusive: false, autoDelete: false);
+    channel.QueueDeclare(queue: "preferential-queue", durable: true, exclusive: false, autoDelete: false);
 
-    channel.ExchangeDeclare("grocery", ExchangeType.Direct);
+    // On a busy day, for example, people who usually don't go to the fast queue have access to it
+    // This situation is for the sake of using the topic exchange
+    var usedExchangeType = ExchangeType.Topic;
+    var usedExchangeName = "grocery";
 
-    channel.QueueBind("fast-queue", "grocery", "fast");
-    channel.QueueBind("normal-queue", "grocery", "normal");
-    channel.QueueBind("preferential-queue", "grocery", "preferential");
+    channel.ExchangeDeclare(usedExchangeName, usedExchangeType);
 
-    return channel;
+    channel.QueueBind("fast-queue", usedExchangeName, "fast");
+    channel.QueueBind("normal-queue", usedExchangeName, "normal");
+    channel.QueueBind("preferential-queue", usedExchangeName, "preferential");
+
+    if (usedExchangeType.Equals(ExchangeType.Topic)) channel.QueueBind("fast-queue", usedExchangeName, "*.busy");
+
+    return (usedExchangeType, usedExchangeName);
 }
