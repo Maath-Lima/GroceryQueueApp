6c2be00d99e6847d771b11e2217b284f0c4305bf
Matheus de Lima Santana <math3226@gmail.com>
2023-09-20 21:33:10 -0300

    Consumer App

---
+++ src/GroceryQueueApp.Consumer.App/Program.cs
@@ -0,0 +1,32 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+    SetupAndRunConsumer(channel, $"Consumer F1", "fast-queue");
+    SetupAndRunConsumer(channel, $"Consumer N1", "normal-queue");
+    SetupAndRunConsumer(channel, $"Consumer P1", "preferential-queue");
+
+    Console.ReadLine();
+}
+
+void SetupAndRunConsumer(IModel channel, string consumerName, string queueName)
+{
+    var consumer = new EventingBasicConsumer(channel);
+
+    consumer.Received += (model, ea) =>
+    {
+        var body = ea.Body;
+        var message = Encoding.UTF8.GetString(body.ToArray());
+
+        Console.WriteLine($"{consumer}: [x] message: {message}");
+
+        //channel.BasicAck(ea.DeliveryTag, false);
+    };
+
+    channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+}
